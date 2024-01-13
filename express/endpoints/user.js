63d8ac75ccdd00aa4ac8ee439376f4c63882eb98
alancooper509 <alancooper509@hotmail.com>
2024-01-12 19:44:46 -0600

    hashing out the functionality of the 'GET user' endpoint with some example users data

--- express/endpoints/user.js
+++ express/endpoints/user.js
@@ -1,5 +1,108 @@
+import levelCounts from '../charts/v1_05_0/levelCounts.json' assert { type: "json" };
+import levelMultipliers from '../constants/levelMultipliers.json' assert { type: "json" };
+import gradeMultipliers from '../constants/gradeMultipliers.json' assert { type: "json" };
+
+import fs from 'fs';
+
 function getUser(req) {
-    return "HI";
+    const name = req.params.name.toUpperCase();
+    const number = req.params.number;
+    const userDir = `./users/${getUserID(name, number)}/`;
+
+    // attempt to load the scores JSON for the requested user
+    let scoresfile, scores;
+    try {
+        scoresfile = fs.readFileSync(`${userDir}/scores.json`, "utf8");
+        scores = JSON.parse(scoresfile);
+    } catch (error) {
+        return {
+            "error": {
+                "code": 404,
+                "message": "User Not Found!"
+            }
+        }
+    }
+
+    const infoFile = fs.readFileSync(`${userDir}/info.json`, "utf8");
+    const info = JSON.parse(infoFile);
+
+    const sortedByLevel = splitByLevel(scores);
+    const sortedScores = getStatistics(sortedByLevel);
+
+    return {
+        "scores": sortedScores,
+        "info": info
+    };
+}
+
+function getUserID(name, number) {
+    return `${name}#${number}`;
+}
+
+function splitByLevel(jsonArray) {
+    let levels = {};
+    for (const idx in jsonArray) {
+        let entry = jsonArray[idx];
+        if (!levels[entry["level"]]) {
+            levels[entry["level"]] = {scores: []};
+        }
+        levels[entry["level"]]["scores"].push(entry);
+    }
+    return levels;
+}
+
+function getStatistics(levelsData) {
+    for (const level in levelsData) {
+        let singlesCount = 0, doublesCount = 0;
+        let averageScore = 0, doublesScore = 0, singlesScore = 0;
+        let totalRating = 0, singlesRating = 0, doublesRating = 0;
+        const entriesForLevel = levelsData[level]["scores"];
+        for (const idx in entriesForLevel) {
+            let entry = entriesForLevel[idx];
+            let scoreEntry = parseInt(entry["score"].replace(/,/g, ''));
+            averageScore += scoreEntry;
+            if (entry["type"] !== 'c' && parseInt(level) >= 10) {
+                totalRating += Math.round(gradeMultipliers[entry["grade"]]*levelMultipliers[level]);
+            }
+
+            switch (entry["type"]) {
+                case 's':
+                    singlesCount += 1;
+                    singlesScore += scoreEntry;
+                    if (parseInt(level) >= 10) {
+                        singlesRating += Math.round(gradeMultipliers[entry["grade"]]*levelMultipliers[level]);
+                    }
+                    break;
+                case 'd':
+                    doublesCount += 1;
+                    doublesScore += scoreEntry;
+                    if (parseInt(level) >= 10) {
+                        doublesRating += Math.round(gradeMultipliers[entry["grade"]]*levelMultipliers[level]);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        averageScore /= entriesForLevel.length;
+        singlesScore /= singlesCount;
+        doublesScore /= doublesCount;
+
+        levelsData[level]["average"] = Math.round(averageScore);
+        levelsData[level]["count"] = entriesForLevel.length;
+        levelsData[level]["rating"] = totalRating;
+        if (isNaN(parseInt(level))) {
+            // co-op charts are difficulty "n<x>" where x is num of players
+            continue;
+        }
+        let totalClearPct   = levelCounts[level][ "total" ] !== 0 ? (entriesForLevel.length / levelCounts[level][ "total" ]).toFixed(4) : 0.00;
+        let singlesClearPct = levelCounts[level]["singles"] !== 0 ?           (singlesCount / levelCounts[level]["singles"]).toFixed(4) : 0.00;
+        let doublesClearPct = levelCounts[level]["doubles"] !== 0 ?           (doublesCount / levelCounts[level]["doubles"]).toFixed(4) : 0.00;
+        levelsData[level]["clear"] = totalClearPct;
+        levelsData[level]["singles"] = {"average": Math.round(singlesScore), "count": singlesCount, "clear": singlesClearPct, "rating": singlesRating};
+        levelsData[level]["doubles"] = {"average": Math.round(doublesScore), "count": doublesCount, "clear": doublesClearPct, "rating": doublesRating};
+    }
+    return levelsData;
 }
 
 export default getUser;
--- express/server.js
+++ express/server.js
@@ -2,12 +2,11 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import fs from 'fs';
 
 // local helpers
 import getUser from './endpoints/user.js';
 
-// code logic
+// script logic
 const app = express();
 app.use(cors());
 const port = 3001;
@@ -15,22 +14,21 @@ const port = 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/api/user/:id/:number', (req, res) => {
+app.get('/api/user/:name/:number', (req, res) => {
   const output = getUser(req);
-  const data = { message: output };
-  // Handle your API logic here
-  // const file = fs.readFileSync("charts/v1_05_0/fullmode.json", "utf8");
-  // const  a = JSON.parse(file);
-  // const data = { message: 'Hello from the server!' };
-  res.json(data);
+  if (output.error) {
+    res.status(output.error.code).send(output.error.message);
+  } else {
+    res.json(output);
+  }
 });
 
-app.post('/api/post', (req, res) => {
-  // Handle your API logic here
-  console.log(req.body);
-  const data = { message: 'Hello from the server!' };
-  res.json(data);
-});
+// app.post('/api/post', (req, res) => {
+//   // Handle your API logic here
+//   console.log(req.body);
+//   const data = { message: 'Hello from the server!' };
+//   res.json(data);
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
