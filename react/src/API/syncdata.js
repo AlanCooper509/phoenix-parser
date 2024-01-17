97977fc5e00d50cebd36f113290105eee407c260
alancooper509 <alancooper509@hotmail.com>
2024-01-16 21:08:46 -0600

    starting to handle response from sync user POST request

--- react/src/API/syncdata.js
+++ react/src/API/syncdata.js
@@ -2,17 +2,38 @@ import axios from 'axios';
 
 import url from './url_params.json';
 
-function postSyncData(name, number, sid) {
+function postSyncData(name, number, submitBtn, sid) {
     const fetchData = async (name, number, sid) => {
         try {
+            // Update state or perform actions with the data
+            if (submitBtn.current) {
+                submitBtn.current.classList.remove("btn-success");
+                submitBtn.current.classList.remove("btn-danger");
+                submitBtn.current.classList.add("btn-secondary");
+                submitBtn.current.firstChild.innerHTML = '';
+                submitBtn.current.firstChild.classList.add("spinner-border", "spinner-border-sm");
+            }
+
             const response = await axios.post(`${url.protocol}://${url.host}:${url.port}/api/sync/${name}/${number}`, {
                 sid: sid
             });
             const responseData = response.data;
 
             // Update state or perform actions with the data
+            if (submitBtn.current) {
+                submitBtn.current.firstChild.classList.remove("spinner-border", "spinner-border-sm");
+                submitBtn.current.classList.add("btn-success");
+                submitBtn.current.classList.remove("btn-secondary");
+                submitBtn.current.firstChild.innerHTML = "Success!";
+            }
             console.log(responseData);
         } catch (error) {
+            if (submitBtn.current) {
+                submitBtn.current.firstChild.classList.remove("spinner-border", "spinner-border-sm");
+                submitBtn.current.firstChild.innerHTML = "Failed";
+                submitBtn.current.classList.add("btn-danger");
+                submitBtn.current.classList.remove("btn-secondary");
+            }
             console.error('Error fetching data:', error);
         }
     };
--- react/src/ResyncForm/ResyncForm.js
+++ react/src/ResyncForm/ResyncForm.js
@@ -1,6 +1,9 @@
 import { useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
+import { BsChevronCompactDown } from "react-icons/bs";
+
 import InfoModal from "./InfoModal";
 import postSyncData from "../API/syncdata";
 
@@ -9,9 +12,11 @@ function ResyncForm() {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const [showForm, setShowForm] = useState(false);
 
     // for submitting
     const sid = useRef(null);
+    const submitBtn = useRef(null);
     const params = useParams();
     const name = params.name.toUpperCase();
     const number = params.number;
@@ -23,27 +28,30 @@ function ResyncForm() {
         if(!sid.current) { return };
         if(!sid.current.value) { return };
         if(sid.current.value.match(/[^a-zA-Z0-9]/g)) { return };
-        postSyncData(name, number, sid.current.value);
+        postSyncData(name, number, submitBtn, sid.current.value);
     }
 
     return (
-        <div className="d-flex flex-column">
-            Update Game Data
-            <div className="d-flex mt-2">
-                <div className="d-flex flex-column align-items-end">
-                    <input ref={sid} type="text" className="form-control" onKeyDown={handleOnEnter} placeholder="Session ID"/>
-                    <div className="d-flex mt-1 justify-content-between w-100">
-                        <small className="form-text link-primary" style={{cursor: "pointer"}} onClick={handleShow}>What is this?</small>
-                        <InfoModal
-                            show={show}
-                            handleClose={handleClose}
-                        />
-                        <Button className="btn btn-sm border-dark btn-secondary ms-2" onClick={handleSubmit}>Sync</Button>
+        <div className="d-flex flex-column align-items-end">
+            <span className="me-2" style={{cursor: "pointer"}} onClick={() => {setShowForm(!showForm)}}>
+                <BsChevronCompactDown style={{transform: `rotate(${showForm ? 180 : 0}deg)`}}/>&nbsp;&nbsp;Update Game Data</span>
+            <Collapse in={showForm}>
+            <div>
+                <div className="d-flex mt-2">
+                    <div className="d-flex flex-column align-items-end">
+                        <input ref={sid} type="text" className="form-control" onKeyDown={handleOnEnter} placeholder="Session ID"/>
+                        <div className="d-flex mt-1 justify-content-between w-100">
+                            <small className="form-text link-primary" style={{cursor: "pointer"}} onClick={handleShow}>What is this?</small>
+                            <InfoModal
+                                show={show}
+                                handleClose={handleClose}
+                            />
+                            <Button ref={submitBtn} className=" btn btn-sm border-dark btn-secondary ms-2" onClick={handleSubmit}><span>Submit</span></Button>
+                        </div>
                     </div>
-                </div>
-                <div>
-                </div>
+                </div>                
             </div>
+            </Collapse>
         </div>
     );
 }
