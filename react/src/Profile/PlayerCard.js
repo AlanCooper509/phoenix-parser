591cbb54e69bca1bb64696dad305d8b8e40b966b
alancooper509 <alancooper509@hotmail.com>
2024-08-17 20:10:19 -0500

    loading wheel for default profile pic during the active user API call, instead of just the default avatar

--- react/src/Profile/PlayerCard.js
+++ react/src/Profile/PlayerCard.js
@@ -2,15 +2,30 @@ import './PlayerCard.css'
 
 function PlayerCard({info}) {
     const titleClasses = "Game-title " + (info && info.title ? info.title.color : "");
-    const profilePic = info && info.avatar ? info.avatar : "https://www.piugame.com/data/avatar_img/4f617606e7751b2dc2559d80f09c40bf.png";
+    let thumbnail = '';
+    if (info.last_updated !== 'Unknown') {
+        let profilePic = "https://www.piugame.com/data/avatar_img/4f617606e7751b2dc2559d80f09c40bf.png";
+        if (info.avatar) {
+            profilePic = info.avatar;
+        }
+        thumbnail = 
+        <img 
+            className="Profile-pic"
+            src={profilePic}
+            alt="Phoenix Profile"
+        />
+    } else {
+        thumbnail = 
+        <div className="Profile-pic d-flex justify-content-center align-items-center">
+            <div className="spinner-border" role="status">
+                <span className="sr-only"></span>
+            </div>
+        </div>
+    }
     return (
     <div className="d-flex justify-content-center align-items-center">
         <a href="/">
-            <img 
-                className="Profile-pic"
-                src={profilePic}
-                alt="Phoenix Profile"
-            />
+            {thumbnail}
         </a>
         <div className="Game-info">
             <div className={titleClasses}>
