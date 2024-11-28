1ef47158704295e6e131d65fc66e64a90f141f5b
alancooper509 <alancooper509@hotmail.com>
2024-11-28 10:50:02 -0600

    accessability update - profile mini cards are <a> tags now

--- react/src/Profile/ProfileMini.js
+++ react/src/Profile/ProfileMini.js
@@ -6,28 +6,31 @@ import splitNameNumber from "../Helpers/splitNameNumber";
 function ProfileMini({info}) {
     function redirect() {
         const user = splitNameNumber(info.player + info.number);
-        window.location.href = `/user/${user.name}/${user.number}`;
+        return `/user/${user.name}/${user.number}`;
     }
     let timestamp = info.last_updated;
     if (info.timestamp) {
         timestamp = new Date(parseInt(info.timestamp) * 1000).toLocaleDateString();
     }
-    
     return (
-        <>
-        <div className="container-mini mt-3" onClick={redirect} style={{"minWidth": "350px", "maxHeight": "115px"}}>
-            <div className="d-flex justify-content-between align-items-center ms-1 w-100">
-                <div className="px-2 pt-2 mb-2 ms-1">
-                    <PlayerCardMini
-                        info={info}
-                    />
-                </div>
+        <a
+          href={redirect()}
+          rel="noopener noreferrer"
+          className="container-mini mt-3 text-decoration-none d-block"
+          style={{ minWidth: '350px', maxHeight: '115px' }}
+        >
+          <div className="d-flex justify-content-between align-items-center ms-1 w-100">
+            <div className="px-2 pt-2 mb-2 ms-1">
+              <PlayerCardMini info={info} />
             </div>
-            <i className="Update-mini position-relative ms-2" style={{bottom: "25px", float: "right", right: "10px"}}>
-                Last Synced: {timestamp}
-            </i>
-        </div>
-        </>
+          </div>
+          <i
+            className="Update-mini position-relative ms-2"
+            style={{ bottom: '25px', float: 'right', right: '10px' }}
+          >
+            Last Synced: {timestamp}
+          </i>
+        </a>
     );
 }
 
